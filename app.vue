be20bf2e365009f2533f74a488da27dbd013d72f
maria <nikmaria15@gmail.com>
2026-03-05 21:44:40 +0500

    initial commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const config = useAppConfig()
+const runtime = useRuntimeConfig()
+const hello = ref(runtime.public.apiurl)
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
