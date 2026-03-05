e132aa96969718498a35163464430f101a385979
maria <nikmaria15@gmail.com>
2026-03-05 21:45:02 +0500

    nuxt config updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  runtimeConfig: {
-    token: '',
-    public: {
-      apiurl: '',
-    },
-  },
 
   modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/image', '@nuxt/scripts'],
 })
